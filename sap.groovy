a83b81b651858789bc147b0aae7546507cbaf5e8
himanshush13 <36809136+himanshush13@users.noreply.github.com>
2020-01-23 12:55:07 +0530

    Update sap.groovy

--- sap.groovy
+++ sap.groovy
@@ -1,72 +1,54 @@
-def abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE) {	
-	println "LABEL=" + LABEL
-	println "HOST=" + HOST
-	println "CREDENTIAL=" + CREDENTIAL
-	println "PACKAGE=" + PACKAGE
-	println "COVERAGE=" + COVERAGE
+import jenkins.model.*
 
-	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		stage('[' + LABEL + '] ABAP Unit') {
-			dir('sap-pipeline') {
-				bat "newman run abap_unit_coverage.postman_collection.json --insecure --bail " +
-				"--environment NPL.postman_environment.json " +
-				"--timeout-request 120000 " +
-				"--global-var host=$HOST " +
-				"--global-var username=$USERNAME " +
-				"--global-var password=$PASSWORD " +
-				"--global-var package=$PACKAGE " +
-				"--global-var coverage_min=$COVERAGE "
-			}
-		}
-	}
-}
+def GITURL = 'https://github.com/himanshush13/abap-ci-postman.git'
+def BRANCH = 'master'
+def PIPELINE_GITURL = 'https://github.com/pacroy/abap-ci-postman.git'
+def PACKAGE = '''$REST_SIMPLE'''
+def COVERAGE = 80
+def VARIANT = "DEFAULT"
 
-def abap_sci(LABEL,HOST,CREDENTIAL,PACKAGE,VARIANT) {	
-	println "LABEL=" + LABEL
-	println "HOST=" + HOST
-	println "CREDENTIAL=" + CREDENTIAL
-	println "PACKAGE=" + PACKAGE
-	println "VARIANT=" + VARIANT
-	
-	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {	
-		stage('[' + LABEL + '] ABAP Code Inspector') {
-			dir('sap-pipeline') {
-					bat "newman run abap_sci.postman_collection.json --insecure --bail " +
-					"--environment NPL.postman_environment.json " +
-					"--timeout-request 120000 " +
-					"--global-var host=$HOST " +
-					"--global-var username=$USERNAME " +
-					"--global-var password=$PASSWORD " +
-					"--global-var package=$PACKAGE " +
-					"--global-var atc_variant=$VARIANT "
-			}
-		}
-	}
-}
-
-def sap_api_test(LABEL,HOST,CREDENTIAL) {
-	println "LABEL=" + LABEL
-	println "HOST=" + HOST
-	println "CREDENTIAL=" + CREDENTIAL
-	
-	withCredentials([usernamePassword(credentialsId: CREDENTIAL, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-		stage('[' + LABEL + '] SAP API Tests') {
-			dir('sap-pipeline') {
-				try {
-					bat "newman run SimpleRESTTest.postman_collection.json --insecure --bail " + 
-					"--environment NPL.postman_environment.json " + 
-					"--reporters junit " +
-					"--timeout-request 10000 " +
-					"--global-var host=$HOST " +
-					"--global-var username=$USERNAME " + 
-					"--global-var password=$PASSWORD "
-				} catch(e) {
-					return 'FAILURE'
-				}
-				junit 'newman/*.xml'
-			}
-		}
-	}
-}
-
-return this
+parallel (
+    "NPL":{
+        node {
+        	def LABEL = "NPL"
+        	def HOST = "vhcalnplci.dummy.nodomain"
+        	def CREDENTIAL = "NPL"
+        	
+        	git poll: true, branch: BRANCH, url: GITURL
+        		
+        	stage('[' + LABEL + '] Preparation') {
+        		deleteDir()
+        		dir('sap-pipeline') {
+        			bat "git clone " + PIPELINE_GITURL + " ."
+        		}
+        	}
+        	
+        	def sap_pipeline = load "sap-pipeline/sap.groovy"
+        	sap_pipeline.abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE)
+        	sap_pipeline.abap_sci(LABEL,HOST,CREDENTIAL,PACKAGE,VARIANT)
+        	sap_pipeline.sap_api_test(LABEL,HOST,CREDENTIAL)
+        }
+    }
+    /* Add more system as needed...
+	,"NPL":{
+        node {
+        	def LABEL = "NPL"
+        	def HOST = "vhcalnplci.dummy.nodomain"
+        	def CREDENTIAL = "NPL"
+        	
+        	git poll: true, branch: BRANCH, url: GITURL
+        		
+        	stage('[' + LABEL + '] Preparation') {
+        		deleteDir()
+        		dir('sap-pipeline') {
+        			bat "git clone " + PIPELINE_GITURL + " ."
+        		}
+        	}
+        	
+        	def sap_pipeline = load "sap-pipeline/sap.groovy"
+        	sap_pipeline.abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE)
+        	sap_pipeline.abap_sci(LABEL,HOST,CREDENTIAL,PACKAGE,VARIANT)
+        	sap_pipeline.sap_api_test(LABEL,HOST,CREDENTIAL)
+        }
+	} */
+)
